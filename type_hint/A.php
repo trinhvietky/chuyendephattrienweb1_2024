6b47caefcbc204ce64ee29bac62fb4f5fde9159f
trinhvietky <josvietky1996@gmail.com>
2024-10-01 21:34:41 +0700

    add lab2 type hint

---
+++ type_hint/A.php
@@ -0,0 +1,8 @@
+<?php
+include ('C.php');
+
+class A extends C {
+    function a1(){
+        echo "This is function a1 from class A";
+    }
+}
---
+++ type_hint/B.php
@@ -0,0 +1,8 @@
+<?php
+include ('C.php');
+
+class B extends C {
+    function b1(){
+        echo "This is function b1 from class B";
+    }
+}
